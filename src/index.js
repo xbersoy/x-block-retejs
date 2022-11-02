aeabe5d63a5cda42df841379e103bfacc4d5140c
Burak Ersoy <burakersoy@msn.com>
2022-11-03 02:07:03 +0300

    change layout

--- src/index.js
+++ src/index.js
@@ -1,30 +1,53 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
 import { createEditor } from "./rete";
-
+import { MuiThemeProvider, createTheme } from "@material-ui/core";
+import { deepPurple, teal } from "@material-ui/core/colors";
+import { Header } from "./components/layouts";
+import { CssBaseline } from "@material-ui/core";
 import "./styles.css";
 
-function App() {
-  const functionName = () => {
-    // console.log(createEditor);
-  };
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: deepPurple[400],
+      light: deepPurple[200],
+      dark: deepPurple[700],
+    },
+    secondary: {
+      main: teal.A400,
+      light: teal[200],
+      dark: teal[700],
+    },
+    type: "dark",
+  },
+});
 
+function App() {
   return (
-    <div className="App">
-      <button onClick={functionName}>Add a new block.</button>
-      <div className="editor">
-        <div className="container">
-          <div
-            className="node-editor"
-            style={{ width: "100vw", height: "100vh" }}
-            ref={(ref) => ref && createEditor(ref)}
-          />
+    <Fragment>
+      <CssBaseline />
+      <Header />
+      <div className="App">
+        <div className="editor">
+          <div className="container">
+            <div
+              className="node-editor"
+              style={{ width: "100vw", height: "100vh" }}
+              ref={(ref) => ref && createEditor(ref)}
+            />
+          </div>
+          <div id="dock" className="dock"></div>
         </div>
-        <div id="dock" className="dock"></div>
       </div>
-    </div>
+    </Fragment>
   );
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+    <App />
+  </MuiThemeProvider>,
+  rootElement
+);
